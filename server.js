1b7bfb225ff7819576be6abb066bccf62fe8c7d1
MalinaliRap <utfprdiario@gmail.com>
2023-07-04 20:06:37 -0300

    Tratativa de erros

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError);
 console.log('API rodando na porta ' + port);
 
 /* normalizando porta (nem sempre a 3000 vai estar disponivel) */
@@ -38,3 +39,26 @@ function normalizePort(val){
 
     return false;
 }
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    const bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
