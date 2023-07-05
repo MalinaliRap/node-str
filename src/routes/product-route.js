914b971f99499e1ae1f377a9b00171f5d318d3dc
MalinaliRap <utfprdiario@gmail.com>
2023-07-04 21:40:11 -0300

    .vscode e eslint para um código mais padronizado | criação dos controllers

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -1,23 +1,13 @@
-'use strict';
+"use strict"
 
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const controller = require("../controllers/product-controller")
 
 /* rotas */
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+router.post("/", controller.post)
+router.put("/:id", controller.put)
+router.delete("/", controller.del)
 
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-module.exports = router;
+module.exports = router
