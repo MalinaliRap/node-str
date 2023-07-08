40719425ff4688780650b0b81d526bb6a413da8e
Gabriel Coplas Becher <utfprdiario@gmail.com>
2023-07-07 22:38:00 -0300

    Conexão com o banco utilizando mongoose | Criação da model de Product

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
 
-const app = express();
-const router = express.Router();
+const app = express()
+const router = express.Router()
+
+// Conecta ao banco
+mongoose.connect("mongodb://mali:mali1284@localhost:27017/admin")
+
+// Carregar modelos
+const Product = require("./models/product")
 
 // Carregar as rotas
-const indexRoute = require('./routes/index-route'); 
-const productRoute = require('./routes/product-route'); 
+const indexRoute = require("./routes/index-route")
+const productRoute = require("./routes/product-route")
 
 /* tratativa de response da requisição */
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
+app.use("/", indexRoute)
+app.use("/products", productRoute)
 
-
-app.use('/', indexRoute);
-app.use('/products', productRoute);
-
-module.exports = app;
+module.exports = app
